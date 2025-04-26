0aa6ac0471b9f7fc83b78c0edbcf3a255e2a3b88
rafaelgp3 <rafaelgp3@al.insper.edu.br>
2025-04-26 18:26:37 -0300

    feat: comçando animações

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
     [SerializeField] private float speed;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -14,6 +16,8 @@ public class PlayerMovement : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
         float moveVertical = Input.GetAxisRaw("Vertical");
+
+        anim.SetFloat("speedY", moveVertical);
 
         Vector2 movement = new(moveHorizontal, moveVertical);
 
