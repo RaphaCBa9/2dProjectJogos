b393650c79ec633febc4380cd137744a63e3345f
rafaelgp3 <rafaelgp3@al.insper.edu.br>
2025-05-04 21:27:08 -0300

    feat: barra de vida

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public float healthPoints;
+    [SerializeField] private TMP_Text healthTxt;
+    [SerializeField] private Slider slider;
+    
+    void Start()
+    {
+        healthPoints = 100;
+    }
+
+    public void HandleMudarSlider(float valor) {
+        healthTxt.SetText(valor.ToString("F0"));
+    }
+
+    public void TomarDano(float dano) {
+        healthPoints -= dano;
+        HandleMudarSlider(healthPoints);
+    }
+}
