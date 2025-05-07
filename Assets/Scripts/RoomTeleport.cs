16c0a7a43df3018e8edc44e2e60c7c92776dbe3f
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2025-05-07 15:43:15 -0300

    feat: teleporte posicao faz sentido

--- Assets/Scripts/RoomTeleport.cs
+++ Assets/Scripts/RoomTeleport.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,12 +11,27 @@ public class RoomTeleport : MonoBehaviour
         if (collision.CompareTag("Player")) {
             // collision.transform.position = new(0f, 4f, 0f);
             // SceneManager.LoadSceneAsync(2);
-            for (int i = 1; i < SceneManager.sceneCount; i++) {
-                // Debug.Log(SceneManager.GetSceneAt(i).name);
-                SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(i).name);
-            }
-            SceneManager.LoadSceneAsync(Random.Range(minRandom, maxRandom+1), LoadSceneMode.Additive);
+            // for (int i = 1; i < SceneManager.sceneCount; i++) {
+            //     // Debug.Log(SceneManager.GetSceneAt(i).name);
+            //     SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(i).name);
+            // }
+            // SceneManager.LoadSceneAsync(Random.Range(minRandom, maxRandom+1), LoadSceneMode.Additive);
+            Debug.Log("Encostou no teleporte");
+            StartCoroutine(LoadScene());
+            // collision.attachedRigidbody.MovePosition(new(collision.transform.position.x, -collision.transform.position.y));
+            collision.transform.position = new(0f, 4f, 0f);
+            Debug.Log("Teleportou");
             // SceneManager.LoadSceneAsync(Random.Range(minRandom, maxRandom+1));
         }
+    }
+
+    private IEnumerator LoadScene() {
+        Debug.Log("Comecou carregar");
+        AsyncOperation unload = SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(1).name);
+        AsyncOperation load = SceneManager.LoadSceneAsync(Random.Range(minRandom, maxRandom+1), LoadSceneMode.Additive);
+        while (!unload.isDone || !load.isDone) {
+            yield return null;
+        }
+        Debug.Log("Terminou carregar");
     }
 }
